3825df713b5b8c56ff5be753c9739b5af9ef6aeb
Brian Galura <Brian.Galura@citrix.com>
2015-09-01 10:56:38 -0700

    create mesos-cloud.conf file locally

--- bin/start.sh
+++ bin/start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash  
 export MY_IP=$(hostname -i)
-export MESOS_IP=${MESOS_IP}
 export LD_LIBRARY_PATH=/lib64:/lib
 set -x
 
+cat << EOF > /mesos-cloud.conf
+[mesos-cloud]
+  mesos-master        = zk://${ZK_IP}:${ZK_PORT}/mesos 
+  http-client-timeout = 5s
+  state-cache-ttl     = 20s
+EOF
+
 /bin/km apiserver \
     --address=${MY_IP} \
-    --etcd-servers=http://${MESOS_IP}:4001 \
+    --etcd-servers=http://${ETCD_IP}:${ETCD_PORT} \
     --service-cluster-ip-range=10.10.10.0/24 \
     --port=8888 \
     --cloud-provider=mesos \
-    --cloud-config=/vagrant/mesos-cloud.conf \
+    --cloud-config=/mesos-cloud.conf \
     --v=5 > /var/log/apiserver.log 2>&1 &
 echo apiserver exit code $?
 
@@ -19,16 +25,16 @@ sleep 5
 /bin/km controller-manager \
     --master=${MY_IP}:8888 \
     --cloud-provider=mesos \
-    --cloud-config=/vagrant/mesos-cloud.conf  \
+    --cloud-config=/mesos-cloud.conf  \
     --v=1 > /var/log/controller.log 2>&1 &
 echo controller exit code $?
 
 sleep 5
 
 /bin/km scheduler \
-    --address=${MY_IP} \
-    --mesos-master=${MESOS_IP}:5050 \
-    --etcd-servers=http://${MESOS_IP}:4001 \
+    --address=0.0.0.0 \
+    --mesos-master=${MESOS_IP}:${MESOS_PORT} \
+    --etcd-servers=http://${ETCD_IP}:${ETCD_PORT} \
     --mesos-user=root \
     --api-servers=${MY_IP}:8888 \
     --cluster-dns=10.10.10.10 \
